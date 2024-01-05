aa6804c515e20da03bee5fbb3d710d631fbae7b8
zakaria-hachem <40008960+zakaria-hachem@users.noreply.github.com>
2024-01-05 17:06:17 +0100

    Update main.rs

--- main/src/img_ack/src/main.rs
+++ main/src/img_ack/src/main.rs
@@ -9,16 +9,5 @@ let _capture_img = binding.arg("-o")
         .status().expect("failed");
 
 
-//let _ = capture_img.arg("-o")
- //       .arg("~/remote/test.png").status().expect("failed");
-// Execute `ls` in the current directory of the program.
-//list_dir.status().expect("process failed to execute");
 
-//println!();
-
-// Change `ls` to execute in the root directory.
-//list_dir.current_dir("/");
-
-// And then execute `ls` again but in the root directory.
-//list_dir.status().expect("process failed to execute");
 }
