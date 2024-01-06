0e92cf7a41c3466b71b560225a66700f5625d924
zakaria-hachem <40008960+zakaria-hachem@users.noreply.github.com>
2024-01-06 18:45:01 +0100

    Delete main/src directory

--- main/src/img_ack/target/debug/deps/img_ack-a5a634e9a97a4edb.d
+++
@@ -1,5 +0,0 @@
-/home/newuser/remote/src/img_ack/target/debug/deps/img_ack-a5a634e9a97a4edb: src/main.rs
-
-/home/newuser/remote/src/img_ack/target/debug/deps/img_ack-a5a634e9a97a4edb.d: src/main.rs
-
-src/main.rs:
--- main/src/img_ack/target/debug/img_ack.d
+++
@@ -1,1 +0,0 @@
-/home/newuser/remote/src/img_ack/target/debug/img_ack: /home/newuser/remote/src/img_ack/src/main.rs
